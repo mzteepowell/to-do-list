e88f16919d6708e1590259da7a1bdb34730e916f
Tarah <tarah.agbokhana@gmail.com>
2021-02-06 06:06:20 -0500

    Added social media handlesbranding

--- to-do-list/src/Footer.js
+++ to-do-list/src/Footer.js
@@ -4,17 +4,40 @@ export default function Footer() {
     return (
     <div>
     <footer className="footer"> 
-    <div className="font-awesome">
-    <i className="far fa-hand-pointer"></i>
-    </div>
-    <a
+    <p>Follow Me</p>
+    <div className="footer-items">
+        <section>
+        <a
         className="App-link"
         href="https://github.com/mzteepowell/to-do-list"
         target="_blank"
         rel="noopener noreferrer"
         >
-        Follow my Github repository
+        <i class="fab fa-github"></i>
         </a>
+        </section>
+        <section>
+        <a
+        className="App-link"
+        href="https://twitter.com/AgbokhanaTarah"
+        target="_blank"
+        rel="noopener noreferrer"
+        >
+        <i class="fab fa-twitter"></i>
+        </a>
+        </section>
+        <section>
+        <a
+        className="App-link"
+        href="https://www.linkedin.com/in/tarah-agbokhana/"
+        target="_blank"
+        rel="noopener noreferrer"
+        >
+        <i class="fab fa-linkedin-in"></i>
+        </a>
+        </section>
+    </div>
+    <p className="branded">powered by Tarah Agbokhana</p>
     </footer>
     </div>
     );
